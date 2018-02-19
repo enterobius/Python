cb7cb22f71ee1c6fa39342c69107702c1ce0db9c
zeodime <zeodime@github.com>
2018-02-19 02:20:38 +0200

    Changed parameters of print() function

--- task2.py
+++ task2.py
@@ -1,5 +1,7 @@
-first_name = 'Alexander'
-patronymic = 'Ivanovich'
-last_name = 'Famusov'
+#Read about which parameters print() takes. It might be more pythonic and faster to calculate.
 
-print(first_name, '|', patronymic, '|', last_name)
+first_name = "Alexander"
+patronymic = "Ivanovich"
+last_name = "Famusov"
+
+print("{0}|{1}|{2}".format(first_name, patronymic, last_name))
